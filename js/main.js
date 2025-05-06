cacfae4dd0a397f73ef11789e6effc708d2b9b34
Huichelaar <acb56334@gmail.com>
2025-05-06 10:58:30 +0200

    Add basic JS
    Added a basic JS script in a separate source file.\n\nAlso added a new page that can be visited from the main page and also links back to the main page.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+/* Displays an alert to the visitor. Args:
+    - message:    Message to be shown to visitor by alert.*/
+function generateAlert(message) {
+  window.alert(message);
+}
+
+generateAlert("Press OK to continue.");
